6c93e415ab4510260816a141b5ea18d67dff6a29
nero <devstory.co.kr@gmail.com>
2020-02-15 01:06:20 +0900

    Update document

--- document.py
+++ document.py
@@ -1,22 +1,38 @@
 import requests, json
 import os, re, csv
 import pandas as pd
+import numpy as np
 from enum import Enum
 from tqdm import trange
 
 class KEYS(Enum):
+    # -1 : 아직 라벨링 안함 (default)
+    # 0  : 개발과 관련없는 문서
+    # 1  : 개발과 관련있는 문서
+    LABEL = 'label'
+    
+    # TAGS + TITLE + DESC
+    TEXT = 'text'
+    
+    # DATA_URL 결과 파싱용 Keys(Beans)
     ID = '_id'
     TITLE = 'title'
     DESC = 'description'
     TAGS = 'tags'
     LINK = 'link'
     
-    def getList():
-        return [k.value for k in KEYS]
+    def getDocKeys():
+        return [KEYS.ID.value, KEYS.TITLE.value, KEYS.DESC.value, KEYS.TAGS.value, KEYS.LINK.value]
+    
+    def getTitleBlackList():
+        return ['', 'about']
+    
+    def getTextKeys():
+        return [KEYS.TAGS.value, KEYS.TITLE.value, KEYS.DESC.value]
 
 class Document():
     
-    def __init__(self, debug=False):
+    def __init__(self, load=True, debug=False):
         self.debug = debug
         
         # Constant
@@ -24,13 +40,14 @@ class Document():
         self.DOCUMENTS_PATH = './data/documents.csv'
         self.MAX_REQ_SIZE = 5000
         
-        if debug: print('> Updating documents...')
-        self._updateDocs()
-        
-        if debug: print('> Loading documents...')
-        self.docs = self._getDocs()
-        
-        if debug: print('> Done!')
+        if load:
+            if debug: print('> Updating documents...')
+            self._updateDocs()
+
+            if debug: print('> Loading documents...')
+            self.data = self._getDocs()
+
+            if debug: print('> Done!')
         
     def _getTotal(self):
         """
@@ -41,7 +58,7 @@ class Document():
         doc = res.json()
         return doc['total'][0]['count']
 
-    def _reqDoc(self, page, size):
+    def _reqDoc(self, page, size, preprocessing=False):
         """
         문서 요청
         - page는 0 부터 시작
@@ -56,7 +73,17 @@ class Document():
         res = requests.get(self.DATA_URL, params)
         res.raise_for_status()
         doc = res.json()
-        return self._preprocessing(doc)
+        
+        # json to dataframe
+        doc = pd.DataFrame(doc['data'], columns=KEYS.getDocKeys())
+        
+        # add label
+        doc.insert(0, KEYS.LABEL.value, -1)
+        
+        if preprocessing:
+            return self._preprocessing(doc)
+        else:
+            return doc
     
     def _preprocessing(self, doc):
         """
@@ -64,28 +91,38 @@ class Document():
         - KEYS 이외의 key 삭제
         - [tag] list join to string
         - [title / description / tags] 영어, 한글, 공백 이외의 것들 모두 삭제
-        - \n, \t 삭제
+        - \ 삭제
         - 2번 이상의 공백 1개로 통합
         - 영어 대문자 소문자로 변환
         - 앞뒤 공백 삭제
         """
-        for data in doc['data']:
-            # KEYS 이외의 key 삭제
-            rm_keys = data.keys() - KEYS.getList()
-            for rm_key in rm_keys:
-                del data[rm_key]
+        # tags
+        doc[KEYS.TAGS.value] = doc[KEYS.TAGS.value].apply(lambda x: ' '.join(x))
             
-            # preprocessing
-            for key in KEYS.getList():
-                if key == 'tags':
-                    data[key] = ' '.join(data[key])
-                if key in ['title', 'description', 'tags']:
-                    data[key] = re.sub('[^가-힣a-zA-Z\s]', '', data[key])
-                data[key] = re.sub('\n', '', data[key])
-                data[key] = re.sub('\t', '', data[key])
-                data[key] = re.sub('\s{2,}', ' ', data[key])
-                data[key] = data[key].lower()
-                data[key] = data[key].strip()
+        # title, description, tags
+        for key in KEYS.getTextKeys():
+            doc[key] = doc[key].apply(lambda x: re.sub('[^가-힣a-zA-Z\s]', '', repr(x)))
+
+        # all
+        def preprocessing(x):
+            text = re.sub(r'\\', '', repr(x))
+            x = re.sub('\s{2,}', ' ', x)
+            x = x.lower()
+            x = x.strip()
+            return x
+
+        for key in KEYS.getDocKeys():
+            doc[key] = doc[key].apply(preprocessing)
+
+        # remove blacklist
+        doc.drop(doc[doc[KEYS.TITLE.value].isin(KEYS.getTitleBlackList())].index).reset_index()
+                        
+        # create text column
+        join_with = lambda x: ' '.join(x.dropna().astype(str))
+        doc[KEYS.TEXT.value] = doc[KEYS.getTextKeys()].apply(
+            join_with,
+            axis=1
+        )
         return doc
         
 
@@ -96,14 +133,14 @@ class Document():
         total = self._getTotal()
         if size > self.MAX_REQ_SIZE: size = self.MAX_REQ_SIZE
         total_req = round(total/size + 0.5)
-        docs = None
+        docs = pd.DataFrame()
         for i in trange(start_page, total_req):
             doc = self._reqDoc(i, size)
-            if docs == None:
+            if docs.empty:
                 docs = doc
             else:
-                docs['data'] += doc['data']
-        return docs
+                docs = docs.append(doc)
+        return self._preprocessing(docs)
 
     def _updateDocs(self):
         """
@@ -116,31 +153,24 @@ class Document():
         if not os.path.isfile(self.DOCUMENTS_PATH):
             # 데이터가 없는 경우
             docs = self._reqDocs(size)
-            with open(self.DOCUMENTS_PATH, 'w') as csv_file:
-                csv_writer = csv.writer(csv_file, delimiter=',')
-                csv_writer.writerow(['_no', '_label'] + KEYS.getList())
-                for i, doc in enumerate(docs['data']):
-                    csv_writer.writerow([i + 1, -1] + [re.sub('\t', '', str(doc[k])) for k in KEYS.getList()])
+            docs.to_csv(self.DOCUMENTS_PATH, sep=",", index=False)
         else:
             # 기존 데이터가 있는 경우
             num_new_docs = 0
-            document = pd.read_csv(self.DOCUMENTS_PATH, delimiter=',')
+            docs = pd.read_csv(self.DOCUMENTS_PATH, delimiter=',')
             total = self._getTotal()
-            old_total = document.tail(1)['_no'].values[0] # 기존 데이터 수
-            new_docs_num = total - old_total
-            if new_docs_num <= 0:
-                if self.debug: print('The document is already up to date.')
-                return
+            total_docs = len(docs)
+            new_docs_num = total - total_docs
+            new_docs = self._reqDocs(size, total_docs // size)
             
-            docs = self._reqDocs(size, old_total // size)
-            no = old_total + 1
-            with open(self.DOCUMENTS_PATH, 'a') as tsv_file:
-                csv_writer = csv.writer(tsv_file, delimiter=',')
-                for i, doc in enumerate(docs['data']):
-                    if doc['_id'] not in document._id.unique():
-                        num_new_docs += 1
-                        csv_writer.writerow([no + i, -1] + [re.sub('\t', '', str(doc[k])) for k in KEYS.getList()])
-            if self.debug: print(f'신규 문서 {num_new_docs}개 추가')
+            # _id가 기존 데이터에 존재하지 않는 경우에만 추가
+            docs.append(new_docs[~new_docs[KEYS.ID.value].isin(docs[KEYS.ID.value])])
+            docs.to_csv(self.DOCUMENTS_PATH, sep=",", index=False)
+            if self.debug:
+                if total_docs == len(docs):
+                    print('문서가 최신입니다.')
+                else:
+                    print(f'신규 문서 {num_new_docs}개 추가')
 
     def _getDocs(self):
         """
@@ -148,22 +178,21 @@ class Document():
         """
         if not os.path.isfile(self.DOCUMENTS_PATH):
             raise FileNotFoundError(f'The scripts file {self.DOCUMENTS_PATH} does not exist')
-        return pd.read_csv(self.DOCUMENTS_PATH, delimiter=',')
+        return pd.read_csv(self.DOCUMENTS_PATH, delimiter=',', dtype={KEYS.LABEL.value: np.int64})
     
-    def syncDocLabel(self, old_document_path, delimiter):
+    def syncDocLabel(self, old_document_path, sep, override=False):
         """
         기존 라벨링한 데이터를 신규 문서에 반영
         """
         def preprocessing(text):
-            text = re.sub('\n', '', text)
-            text = re.sub('\t', '', text)
+            text = re.sub(r'\\', '', text)
             text = re.sub('\s{2,}', ' ', text)
             text = text.lower()
             text = text.strip()
             return text
         
         document = pd.read_csv(self.DOCUMENTS_PATH, delimiter=',')
-        old_document = pd.read_csv(old_document_path, delimiter=delimiter)
+        old_document = pd.read_csv(old_document_path, delimiter=sep)
         for index, row in old_document[:10000].iterrows():
             link = preprocessing(row.link)
             title = re.sub('[^가-힣a-zA-Z\s]', '', row.title)
@@ -173,10 +202,11 @@ class Document():
             if not len(document.loc[document.title.str.strip() == title.strip()]) and not len(document.loc[document.link == link]):
                 print(f'not found : {row.title}')
             elif len(document.loc[document.title.str.strip() == title.strip()]):
-                document.loc[document.title.str.strip() == title.strip(), '_label'] = label
+                document.loc[document.title.str.strip() == title.strip(), KEYS.LABEL.value] = label
             elif len(document.loc[document.link == link]):
-                document.loc[document.link == link, '_label'] = label
+                document.loc[document.link == link, KEYS.LABEL.value] = label
         
         # save synchronized document
-        document.to_csv(self.DOCUMENTS_PATH, sep=",", index=False)
-        return document
+        if override:
+            document.to_csv(self.DOCUMENTS_PATH, sep=",", index=False)
+        print('done')
