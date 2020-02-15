6b5ca4407bc7452553df117bdd1afc53bc67511f
nero <devstory.co.kr@gmail.com>
2020-02-15 14:12:14 +0900

    Add util

---
+++ util.py
@@ -0,0 +1,13 @@
+import urllib.request
+from tqdm import tqdm
+
+def downloadByURL(url, output_path):
+    class DownloadProgressBar(tqdm):
+        def update_to(self, b=1, bsize=1, tsize=None):
+            if tsize is not None:
+                self.total = tsize
+            self.update(b * bsize - self.n)
+        
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
